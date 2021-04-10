f74200f9c7ffc91fa1fcf5dc0c9e583b05035eb9
Izabel Vidal <izabelvidal99@gmail.com>
2021-04-10 13:44:37 -0300

    first commit

---
+++ src/test/java/br/edu/ufape/crud/produto/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.crud.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
